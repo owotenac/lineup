3b13f508c3cfd1f9d67d1ef1297bd9b7a737159d
Olivier Woignier <owoignier@slb.com>
2023-10-05 23:28:26 +0200

    remove fs:promise

--- server2.js
+++ server2.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
-const { read } = require("fs");
+//const { read } = require("fs");
 const app = express();
 const  _readPlayers = require("./players.js");
 const renderModule = require("./render");
